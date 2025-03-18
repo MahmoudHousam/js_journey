a687de0300cf3b836a13805c149954dfac45785a
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-03-18 19:04:38 +0200

    coercion

---
+++ 05_data_types/coercion.js
@@ -0,0 +1,4 @@
+console.log(5 * "5");
+console.log(3 + "15"); // this will be interpreted as string concatenation (315 not 18)
+console.log(20 / "4");
+console.log(33 - "19");
---
+++ 05_data_types/objects.js
@@ -0,0 +1,10 @@
+let person = {
+  name: "mahmoud",
+  age: 30,
+  isStudent: false,
+};
+
+let arr = [1, 2, 3];
+
+console.log(typeof person);
+console.log(typeof arr);

0ea8cacafca618c8efab0a3232c43c153ba8fd69
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-03-19 05:15:08 +0200

    06_type_conversions

---
+++ 06_type_conversions/to_string.js
@@ -0,0 +1,7 @@
+let xyz = 123;
+let bool = true;
+console.log(typeof xyz);
+console.log(typeof String(xyz));
+console.log(typeof bool);
+console.log(typeof bool.toString());
+console.log((14.3).toString());

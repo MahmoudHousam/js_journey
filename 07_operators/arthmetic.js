c3aa0b444330d16fc6d77afb175ea099ab1baf38
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-03-19 22:46:43 +0200

    string concatenation

---
+++ 07_operators/arthmetic.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// Increment and Decrement
+let c = 5;
+console.log(c++);
+console.log(++c);
+console.log(c--);
+console.log(--c);
+
+// String concatenation
+let str1 = "Hello";
+let str2 = "World";
+console.log(str1 + " " + str2);
---
+++ 08_string_concatenation/template_literals.js
@@ -0,0 +1,10 @@
+let myName = "Mahmoud";
+let myAge = 30;
+console.log(`My name is ${myName} and I am ${myAge} years old`);
+
+let multiline = `
+This is a multiline script
+to introduce myself: I am ${myName}.
+I am ${myAge} years old 
+`;
+console.log(multiline);
